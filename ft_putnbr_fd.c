7a678279e12282cb62bd3ed3897cce1a01b5bf95
nclabaux <nclabaux@student.42.fr>
2019-11-21 13:14:55 +0100

    ok

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nclabaux <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/25 18:12:35 by nclabaux          #+#    #+#             */
+/*   Updated: 2019/11/15 12:15:57 by nclabaux         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static void	zero(int fd)
+{
+	write(fd, "0", 1);
+}
+
+static void	size(long x, size_t *len)
+{
+	*len = 0;
+	while (x)
+	{
+		x = x / 10;
+		(*len)++;
+	}
+}
+
+void		ft_putnbr_fd(int n, int fd)
+{
+	long	x;
+	size_t	i;
+	size_t	len;
+	char	stock[11];
+
+	if (n == 0)
+		return (zero(fd));
+	x = n;
+	if (x < 0)
+		x = -x;
+	size(x, &len);
+	i = 0;
+	while (x)
+	{
+		stock[i++] = x % 10 + '0';
+		x = x / 10;
+	}
+	if (n < 0)
+	{
+		stock[i] = '-';
+		len++;
+	}
+	while (len)
+		write(fd, &stock[--len], 1);
+}
